437d7356a17cd8eb5aa2d10f301d78ec4c7ab501
marimari <marabloo02@gmail.com>
2021-02-17 09:10:57 -0600

    laboratorio 1

---
+++ Laboratorio_1.R
@@ -0,0 +1,21 @@
+# Gastos totales
+# voy a ingresar los gastos totales de un universitario 
+300 + 240 + 1527 + 400 + 1500 + 1833
+
+
+celular <- 300
+
+
+
+celular
+Transporte <- 240
+Transporte
+comestibles <- 1527
+comestibles
+gimnasio <- 400
+gimnasio
+alquiler <- 1500
+alquiler
+otros <- 1833
+otros
+
