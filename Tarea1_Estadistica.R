842a5c05b9232f8c01e4c03d54bbb5cac8293935
marimari <marabloo02@gmail.com>
2021-02-21 20:42:55 -0600

    Problemas

---
+++ Tarea1_Estadistica.R
@@ -0,0 +1,15 @@
+# Maria Fernanda Viveros Segovia 1917915
+
+#Superficie 
+31440 + 1453 + 450 + 1200 + 720
+Pinus <- 3140
+Mezquite <- 1453
+Encino <- 450 
+Teka <- 1200
+Juniperos <- 720
+Superficie <- c(Pinus, Mezquite, Encino, Teka, Juniperos)
+barplot(Superficie, col = "purple", ylab = "superficie", xlab = "especie",
+        main = "Barplot de superficie")
+mean(Superficie)
+
+# Marco Aurelio Gonzalez Tagle. (2021, February). Cuadernillo de Ejercicios Principios de estadistica Ingenerio Forestal (Version 1).
---
+++ Tarea1_Problema2.R
@@ -0,0 +1,4 @@
+# Maria Fernanda Viveros 1917915
+germinacion <- c(4, 1, 6, 2, 4, 2, 4, 2, 4, 6, 3, 5, 3, 2, 5, 4, 0, 5, 4, 2, 4, 5, 3, 5, 3, 5, 4, 3, 6, 2)
+mean(germinacion)
+sd(germinacion)
